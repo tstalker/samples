1fb253f78de252f48520744a8f79e8b145c41af6
Stalker <ttstalker@gmail.com>
2022-03-03 22:17:22 +0200

    types

--- types1/main.cpp
+++ types1/main.cpp
@@ -6,72 +6,72 @@
 int main(void)
 {
 	std::cout << "bool: " << sizeof(bool)
-		<< " min: " << std::numeric_limits <bool> ::min()
-		<< " max: " << std::numeric_limits <bool> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<bool>::min()
+		<< " max: " << std::numeric_limits<bool>::max() << std::endl;
 	std::cout << "byte: " << sizeof(std::byte)
-		<< " min: " << static_cast <std::size_t> (std::numeric_limits <unsigned char> ::min())
-		<< " max: " << static_cast <std::size_t> (std::numeric_limits <unsigned char> ::max()) << std::endl;
+		<< " min: " << static_cast<std::size_t>(std::numeric_limits<unsigned char>::min())
+		<< " max: " << static_cast<std::size_t>(std::numeric_limits<unsigned char>::max()) << std::endl;
 
 	std::cout << "char: " << sizeof(char)
-		<< " min: " << static_cast <int> (std::numeric_limits <char> ::min())
-		<< " max: " << static_cast <int> (std::numeric_limits <char> ::max()) << std::endl;
+		<< " min: " << static_cast<int>(std::numeric_limits<char>::min())
+		<< " max: " << static_cast<int>(std::numeric_limits<char>::max()) << std::endl;
 	std::cout << "signed char: " << sizeof(signed char)
-		<< " min: " << static_cast <int> (std::numeric_limits <signed char> ::min())
-		<< " max: " << static_cast <int> (std::numeric_limits <signed char> ::max()) << std::endl;
+		<< " min: " << static_cast<int>(std::numeric_limits<signed char>::min())
+		<< " max: " << static_cast<int>(std::numeric_limits<signed char>::max()) << std::endl;
 	std::cout << "unsigned char: " << sizeof(unsigned char)
-		<< " min: " << static_cast <std::size_t> (std::numeric_limits <unsigned char> ::min())
-		<< " max: " << static_cast <std::size_t> (std::numeric_limits <unsigned char> ::max()) << std::endl;
+		<< " min: " << static_cast<std::size_t>(std::numeric_limits<unsigned char>::min())
+		<< " max: " << static_cast<std::size_t>(std::numeric_limits<unsigned char>::max()) << std::endl;
 
 	std::cout << "char8_t: " << sizeof(char8_t)
-		<< " min: " << static_cast <std::size_t> (std::numeric_limits <char8_t> ::min())
-		<< " max: " << static_cast <std::size_t> (std::numeric_limits <char8_t> ::max()) << std::endl;
+		<< " min: " << static_cast<std::size_t>(std::numeric_limits<char8_t>::min())
+		<< " max: " << static_cast<std::size_t>(std::numeric_limits<char8_t>::max()) << std::endl;
 	std::cout << "char16_t: " << sizeof(char16_t)
-		<< " min: " << static_cast <std::size_t> (std::numeric_limits <char16_t> ::min())
-		<< " max: " << static_cast <std::size_t> (std::numeric_limits <char16_t> ::max()) << std::endl;
+		<< " min: " << static_cast<std::size_t>(std::numeric_limits<char16_t>::min())
+		<< " max: " << static_cast<std::size_t>(std::numeric_limits<char16_t>::max()) << std::endl;
 	std::cout << "char32_t: " << sizeof(char32_t)
-		<< " min: " << static_cast <std::size_t> (std::numeric_limits <char32_t> ::min())
-		<< " max: " << static_cast <std::size_t> (std::numeric_limits <char32_t> ::max()) << std::endl;
+		<< " min: " << static_cast<std::size_t>(std::numeric_limits<char32_t>::min())
+		<< " max: " << static_cast<std::size_t>(std::numeric_limits<char32_t>::max()) << std::endl;
 	std::cout << "wchar_t: " << sizeof(wchar_t)
-		<< " min: " << static_cast <std::size_t> (std::numeric_limits <wchar_t> ::min())
-		<< " max: " << static_cast <std::size_t> (std::numeric_limits <wchar_t> ::max()) << std::endl;
+		<< " min: " << static_cast<std::size_t>(std::numeric_limits<wchar_t>::min())
+		<< " max: " << static_cast<std::size_t>(std::numeric_limits<wchar_t>::max()) << std::endl;
 
 	std::cout << "short: " << sizeof(short)
-		<< " min: " << std::numeric_limits <short> ::min()
-		<< " max: " << std::numeric_limits <short> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<short>::min()
+		<< " max: " << std::numeric_limits<short>::max() << std::endl;
 	std::cout << "unsigned short: " << sizeof(unsigned short)
-		<< " min: " << std::numeric_limits <unsigned short> ::min()
-		<< " max: " << std::numeric_limits <unsigned short> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<unsigned short>::min()
+		<< " max: " << std::numeric_limits<unsigned short>::max() << std::endl;
 
 	std::cout << "int: " << sizeof(int)
-		<< " min: " << std::numeric_limits <int> ::min()
-		<< " max: " << std::numeric_limits <int> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<int>::min()
+		<< " max: " << std::numeric_limits<int>::max() << std::endl;
 	std::cout << "unsigned int: " << sizeof(unsigned)
-		<< " min: " << std::numeric_limits <unsigned> ::min()
-		<< " max: " << std::numeric_limits <unsigned> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<unsigned>::min()
+		<< " max: " << std::numeric_limits<unsigned>::max() << std::endl;
 
 	std::cout << "long: " << sizeof(long)
-		<< " min: " << std::numeric_limits <long> ::min()
-		<< " max: " << std::numeric_limits <long> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<long>::min()
+		<< " max: " << std::numeric_limits<long>::max() << std::endl;
 	std::cout << "unsigned long: " << sizeof(unsigned long)
-		<< " min: " << std::numeric_limits <unsigned long> ::min()
-		<< " max: " << std::numeric_limits <unsigned long> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<unsigned long>::min()
+		<< " max: " << std::numeric_limits<unsigned long>::max() << std::endl;
 
 	std::cout << "long long: " << sizeof(long long)
-		<< " min: " << std::numeric_limits <long long> ::min()
-		<< " max: " << std::numeric_limits <long long> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<long long>::min()
+		<< " max: " << std::numeric_limits<long long>::max() << std::endl;
 	std::cout << "unsigned long long: " << sizeof(unsigned long long)
-		<< " min: " << std::numeric_limits <unsigned long long> ::min()
-		<< " max: " << std::numeric_limits <unsigned long long> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<unsigned long long>::min()
+		<< " max: " << std::numeric_limits<unsigned long long>::max() << std::endl;
 
 	std::cout << "float: " << sizeof(float)
-		<< " min: " << std::numeric_limits <float> ::min()
-		<< " max: " << std::numeric_limits <float> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<float>::min()
+		<< " max: " << std::numeric_limits<float>::max() << std::endl;
 	std::cout << "double: " << sizeof(double)
-		<< " min: " << std::numeric_limits <double> ::min()
-		<< " max: " << std::numeric_limits <double> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<double>::min()
+		<< " max: " << std::numeric_limits<double>::max() << std::endl;
 	std::cout << "long double: " << sizeof(long double)
-		<< " min: " << std::numeric_limits <long double> ::min()
-		<< " max: " << std::numeric_limits <long double> ::max() << std::endl;
+		<< " min: " << std::numeric_limits<long double>::min()
+		<< " max: " << std::numeric_limits<long double>::max() << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- types2/print.hpp
+++ types2/print.hpp
@@ -1,8 +1,4 @@
-#ifdef PRINT_HPP
-#error Redefined header print.hpp
-#endif
-
-#define PRINT_HPP
+#pragma once
 
 #include <limits>
 #include <cstddef>
@@ -18,128 +14,128 @@ template <typename T>
 template <typename T, typename U>
 	void print(void);
 template <typename T, typename U>
-	void print(const pair <U, U> &);
+	void print(const pair<U, U>&);
 template <typename T>
 	constexpr string_view gettype(void);
 template <typename T, typename U>
-	constexpr pair <U, U> getlim(void);
+	constexpr pair<U, U> getlim(void);
 }
 
 template <typename T>
 void prn::print(void)
 {
-	print <T, T> ();
+	print<T, T>();
 }
 
 template <>
-void prn::print <std::byte> (void)
+void prn::print<std::byte>(void)
 {
-	print <byte, unsigned short> ();
+	print<byte, unsigned short>();
 }
 
 template <>
-void prn::print <bool> (void)
+void prn::print<bool>(void)
 {
-	print <bool, string_view> ({ "false", "true" });
+	print<bool, string_view>({"false", "true"});
 }
 
 template <>
-void prn::print <char> (void)
+void prn::print<char>(void)
 {
-	print <char, short> ();
+	print<char, short>();
 }
 
 template <>
-void prn::print <signed char> (void)
+void prn::print<signed char>(void)
 {
-	print <signed char, short> ();
+	print<signed char, short>();
 }
 
 template <>
-void prn::print <unsigned char> (void)
+void prn::print<unsigned char>(void)
 {
-	print <unsigned char, unsigned short> ();
+	print<unsigned char, unsigned short>();
 }
 
 template <>
-void prn::print <char8_t> (void)
+void prn::print<char8_t>(void)
 {
-	print <char8_t, unsigned short> ();
+	print<char8_t, unsigned short>();
 }
 
 template <>
-void prn::print <char16_t> (void)
+void prn::print<char16_t>(void)
 {
-	print <char16_t, unsigned short> ();
+	print<char16_t, unsigned short>();
 }
 
 template <>
-void prn::print <char32_t> (void)
+void prn::print<char32_t>(void)
 {
-	print <char32_t, unsigned long> ();
+	print<char32_t, unsigned long>();
 }
 
 template <>
-void prn::print <wchar_t> (void)
+void prn::print<wchar_t>(void)
 {
-	print <wchar_t, unsigned long> ();
+	print<wchar_t, unsigned long>();
 }
 
 template <typename T, typename U>
 void prn::print(void)
 {
-	print <T, U> (getlim <T, U> ());
+	print<T, U>(getlim<T, U>());
 }
 
 template <typename T, typename U>
-void prn::print(const pair <U, U> &p)
+void prn::print(const pair<U, U> &p)
 {
-	cout << gettype <T> () << ": " << sizeof(T) << " min: " << p.first << " max: " << p.second << endl;
+	cout << gettype<T>() << ": " << sizeof(T) << " min: " << p.first << " max: " << p.second << endl;
 }
 
 template <typename T>
 constexpr auto prn::gettype(void)
 -> string_view
 {
-	if constexpr(is_same_v <T, bool>)
+	if constexpr(is_same_v<T, bool>)
 		return "bool";
-	else if constexpr(is_same_v <T, char>)
+	else if constexpr(is_same_v<T, char>)
 		return "char";
-	else if constexpr(is_same_v <T, signed char>)
+	else if constexpr(is_same_v<T, signed char>)
 		return "signed char";
-	else if constexpr(is_same_v <T, unsigned char>)
+	else if constexpr(is_same_v<T, unsigned char>)
 		return "unsigned char";
-	else if constexpr(is_same_v <T, byte>)
+	else if constexpr(is_same_v<T, byte>)
 		return "byte";
-	else if constexpr(is_same_v <T, char8_t>)
+	else if constexpr(is_same_v<T, char8_t>)
 		return "char8_t";
-	else if constexpr(is_same_v <T, char16_t>)
+	else if constexpr(is_same_v<T, char16_t>)
 		return "char16_t";
-	else if constexpr(is_same_v <T, char32_t>)
+	else if constexpr(is_same_v<T, char32_t>)
 		return "char32_t";
-	else if constexpr(is_same_v <T, wchar_t>)
+	else if constexpr(is_same_v<T, wchar_t>)
 		return "wchar_t";
-	else if constexpr(is_same_v <T, short>)
+	else if constexpr(is_same_v<T, short>)
 		return "short";
-	else if constexpr(is_same_v <T, unsigned short>)
+	else if constexpr(is_same_v<T, unsigned short>)
 		return "unsigned short";
-	else if constexpr(is_same_v <T, int>)
+	else if constexpr(is_same_v<T, int>)
 		return "int";
-	else if constexpr(is_same_v <T, unsigned>)
+	else if constexpr(is_same_v<T, unsigned>)
 		return "unsigned int";
-	else if constexpr(is_same_v <T, long>)
+	else if constexpr(is_same_v<T, long>)
 		return "long";
-	else if constexpr(is_same_v <T, unsigned long>)
+	else if constexpr(is_same_v<T, unsigned long>)
 		return "unsigned long";
-	else if constexpr(is_same_v <T, long long>)
+	else if constexpr(is_same_v<T, long long>)
 		return "long long";
-	else if constexpr(is_same_v <T, unsigned long long>)
+	else if constexpr(is_same_v<T, unsigned long long>)
 		return "unsigned long long";
-	else if constexpr(is_same_v <T, float>)
+	else if constexpr(is_same_v<T, float>)
 		return "float";
-	else if constexpr(is_same_v <T, double>)
+	else if constexpr(is_same_v<T, double>)
 		return "double";
-	else if constexpr(is_same_v <T, long double>)
+	else if constexpr(is_same_v<T, long double>)
 		return "long double";
 	else
 		return "Error: wrong type";
@@ -147,14 +143,14 @@ constexpr auto prn::gettype(void)
 
 template <typename T, typename U>
 constexpr auto prn::getlim(void)
--> pair <U, U>
+-> pair<U, U>
 {
-	return { numeric_limits <T> ::min(), numeric_limits <T> ::max() };
+	return {numeric_limits<T>::min(), numeric_limits<T>::max()};
 }
 
 template <>
-constexpr auto prn::getlim <std::byte, unsigned short> (void)
--> pair <unsigned short, unsigned short>
+constexpr auto prn::getlim<std::byte, unsigned short>(void)
+-> pair<unsigned short, unsigned short>
 {
-	return getlim <unsigned char, unsigned short> ();
+	return getlim<unsigned char, unsigned short>();
 }
