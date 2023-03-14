2f102ec13fc00572b383a7705e04e84b2c4e54ad
stalker <ttstalker@gmail.com>
2023-03-14 13:21:25 +0200

    timecur1

--- timecur/main.cpp
+++ timecur/main.cpp
@@ -6,6 +6,6 @@ int main(void)
 	const auto now(std::chrono::system_clock::now());
 	const auto ttime(std::chrono::system_clock::to_time_t(now));
 	const auto ctime(std::ctime(&ttime));
-	std::cout << ctime << std::endl;
+	std::cout << ctime;
 	return EXIT_SUCCESS;
 }
