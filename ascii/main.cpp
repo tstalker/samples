ae76fe7d1c87036e6fe91dafdc765e486b28f653
stalker <ttstalker@gmail.com>
2026-04-05 11:02:11 +0300

    ascii

--- ascii/main.cpp
+++ ascii/main.cpp
@@ -1,17 +1,17 @@
-#include <limits>
+#include <cctype>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 
 int main(void)
 {
 	auto first(true);
-
-	for(short c(std::numeric_limits<char>::min()); c <= std::numeric_limits<char>::max(); c++)
+	for(short i(std::numeric_limits<char>::min()); i <= std::numeric_limits<char>::max(); i++)
 	{
-		const auto d(std::iscntrl(c) ? ' ' : c);
-		const unsigned char e(c);
-		const unsigned short u(e);
-		std::cout.put(d) << ':' << u;
-
+		if(!std::isprint(i))
+		{
+			continue;
+		}
 		if(first)
 		{
 			first = false;
@@ -20,6 +20,8 @@ int main(void)
 		{
 			std::cout << ' ';
 		}
+		const auto c(static_cast<char>(i));
+		std::cout << std::hex << i << ':' << c;
 	}
 	std::cout << std::endl;
 	return EXIT_SUCCESS;
