16120a40e48ce992ae9e93463b0fe98bd78645a7
stalker <ttstalker@gmail.com>
2026-04-07 17:52:07 +0300

    mkinclude/gendefs.mk

--- mkinclude/gendefs.mk
+++ mkinclude/gendefs.mk
@@ -1,6 +1,6 @@
 DEPENDS = Makefile
 
-AFLAGS = -flto=4 -march=native -O3 -pipe
+AFLAGS = -flto=auto -march=native -O3 -pipe
 BFLAGS = $(AFLAGS) -std=$(STD)
 WFLAGS = -Wall -Werror -Winline
 CFLAGS = $(BFLAGS) $(WFLAGS)
